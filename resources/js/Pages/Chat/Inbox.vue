a7983fe2311c0c5da6714c7b6341e2a3cae1eb84
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2024-07-03 12:54:37 +0600

    real time chat fix

--- resources/js/Pages/Chat/Inbox.vue
+++ resources/js/Pages/Chat/Inbox.vue
@@ -1,52 +1,3 @@
-<template>
-    <Head title="Chat" />
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Chat</h2>
-        </template>
-        <div>
-            <div class="flex flex-col justify-end h-80">
-                <div ref="messagesContainer" class="p-4 overflow-y-auto max-h-fit">
-                    <div
-                        v-for="message in messages"
-                        :key="message.id"
-                        class="flex items-center mb-2"
-                    >
-                        <div
-                            v-if="message.sender_id === currentUser.id"
-                            class="p-2 ml-auto text-white bg-blue-500 rounded-lg"
-                        >
-                            {{ message.text }}
-                        </div>
-                        <div v-else class="p-2 mr-auto bg-gray-200 rounded-lg">
-                            {{ message.text }}
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="flex items-center">
-                <input
-                    type="text"
-                    v-model="newMessage"
-                    @keydown="sendTypingEvent"
-                    @keyup.enter="sendMessage"
-                    placeholder="Type a message..."
-                    class="flex-1 px-2 py-1 border rounded-lg"
-                />
-                <button
-                    @click="sendMessage"
-                    class="px-4 py-1 ml-2 text-white bg-blue-500 rounded-lg"
-                >
-                    Send
-                </button>
-            </div>
-            <small v-if="isFriendTyping" class="text-gray-700">
-                {{ friend.name }} is typing...
-            </small>
-        </div>
-    </AuthenticatedLayout>
-</template>
-
 <script setup>
 import axios from "axios";
 import {nextTick, onMounted, ref, watch} from "vue";
@@ -97,23 +48,25 @@ const sendMessage = () => {
 };
 
 const sendTypingEvent = () => {
+    console.log(props.friend.id, props.currentUser.id, 'type event')
     Echo.private(`chat.${props.friend.id}`).whisper("typing", {
         userID: props.currentUser.id,
     });
-    console.log(props.currentUser.id, 'id')
 };
 
 onMounted(() => {
     axios.get(`/messages/${props.friend.id}`).then((response) => {
-        console.log(response.data);
         messages.value = response.data;
     });
 
+    console.log(props.currentUser.id, 'currentUser id')
     Echo.private(`chat.${props.currentUser.id}`)
-        .listen("MessageSent", (response) => {
+        .listen("MessageSentEvent", (response) => {
+            console.log(response, 'MessageSentEvent response')
             messages.value.push(response.message);
         })
         .listenForWhisper("typing", (response) => {
+            console.log(response, 'sssss')
             isFriendTyping.value = response.userID === props.friend.id;
 
             if (isFriendTypingTimer.value) {
@@ -126,3 +79,54 @@ onMounted(() => {
         });
 });
 </script>
+
+<template>
+    <Head title="Chat" />
+
+    <AuthenticatedLayout>
+    <div>
+        <div class="flex flex-col justify-end h-80">
+            <div ref="messagesContainer" class="p-4 overflow-y-auto max-h-fit">
+                <div
+                    v-for="message in messages"
+                    :key="message.id"
+                    class="flex items-center mb-2"
+                >
+                    <div
+                        v-if="message.sender_id === currentUser.id"
+                        class="p-2 ml-auto text-white bg-blue-500 rounded-lg"
+                    >
+                        {{ message.text }}
+                    </div>
+                    <div v-else class="p-2 mr-auto bg-gray-200 rounded-lg">
+                        {{ message.text }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex items-center">
+            <input
+                type="text"
+                v-model="newMessage"
+                @keydown="sendTypingEvent"
+                @keyup.enter="sendMessage"
+                placeholder="Type a message..."
+                class="flex-1 px-2 py-1 border rounded-lg"
+            />
+            <button
+                @click="sendMessage"
+                class="px-4 py-1 ml-2 text-white bg-blue-500 rounded-lg"
+            >
+                Send
+            </button>
+        </div>
+        <small v-if="isFriendTyping" class="text-gray-700">
+            {{ friend.name }} is typing...
+        </small>
+    </div>
+    </AuthenticatedLayout>
+</template>
+
+<style scoped>
+
+</style>
